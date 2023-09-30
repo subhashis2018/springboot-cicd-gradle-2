7b84abd87f6c60bd8db4fb3e08f01d2f98e1e0ac
subhashis <subhashis.sarangi@gmail.com>
2023-09-30 19:17:47 +0530

    commit

---
+++ src/main/java/com/myapp/springbootcicdgradle2/SpringbootCicdGradle2Application.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootcicdgradle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdGradle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdGradle2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/springbootcicdgradle2/SpringbootCicdGradle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootcicdgradle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdGradle2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
